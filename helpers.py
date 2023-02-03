6c1d1702d7a1282e77f9c7c023d872a58aad967a
jrockholt3 <32277843+jrockholt3@users.noreply.github.com>
2023-02-02 23:28:41 -0500

    02-01 Started Building Network

---
+++ helpers.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def quantize(arr, res=0.01, workspace_limits=np.array([[-.6,.6],[-.6,.6],[0,.9]])):
+    # this helper function takes in a 3xN array of (x,y,z) coords and
+    # outputs the ndx of the coord based on a array representing the whole workspace
+    # with resolution: "res"
+    range_ = np.abs(workspace_limits[:,1] - workspace_limits[:,0])
+    ndx_range = range_/res 
+    ndx = np.round(ndx_range * (arr - workspace_limits[:,0]) / range_)
+    return np.int16(ndx)   
